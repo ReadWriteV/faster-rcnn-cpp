4365925b201a7cb5fb4d4caa45f42c845ef1cb41
wr <wr666@mail.ustc.edu.cn>
2022-10-25 10:08:55 +0000

    naming each module in vgg16

--- backbone/vgg.cpp
+++ backbone/vgg.cpp
@@ -6,19 +6,42 @@
 namespace backbone
 {
 
-VGGImpl::VGGImpl(torch::nn::Sequential features, int num_classes, bool init_weights)
+VGGImpl::VGGImpl(int num_classes, bool init_weights)
 {
-    this->features = features;
+    const std::array cfg_D = {
+        64, 64, -1 /* stands for M */, 128, 128, -1, 256, 256, 256, -1, 512, 512, 512, -1, 512, 512, 512, -1};
+
+    int in_channels = 3;
+    int count = 0;
+    for (auto v : cfg_D)
+    {
+        count++;
+        if (v == -1) // stands for M
+        {
+            this->features->push_back("MaxPool2d" + std::to_string(count),
+                                      torch::nn::MaxPool2d(torch::nn::MaxPool2dOptions(2)));
+        }
+        else
+        {
+            this->features->push_back("Conv2d" + std::to_string(count),
+                                      torch::nn::Conv2d(torch::nn::Conv2dOptions(in_channels, v, 3).padding(1)));
+            this->features->push_back("ReLU" + std::to_string(count), torch::nn::ReLU(torch::nn::ReLUOptions(true)));
+            in_channels = v;
+        }
+    }
+
     this->avgpool = torch::nn::AdaptiveAvgPool2d(torch::nn::AdaptiveAvgPool2dOptions(7));
-    this->classifier->push_back(torch::nn::Linear(torch::nn::LinearOptions(512 * 7 * 7, 4096)));
-    this->classifier->push_back(torch::nn::ReLU(torch::nn::ReLUOptions(true)));
-    this->classifier->push_back(torch::nn::Dropout());
-    this->classifier->push_back(torch::nn::Linear(torch::nn::LinearOptions(4096, 4096)));
-    this->classifier->push_back(torch::nn::ReLU(torch::nn::ReLUOptions(true)));
-    this->classifier->push_back(torch::nn::Dropout());
-    this->classifier->push_back(torch::nn::Linear(torch::nn::LinearOptions(4096, num_classes)));
+
+    this->classifier->push_back("Linear1", torch::nn::Linear(torch::nn::LinearOptions(512 * 7 * 7, 4096)));
+    this->classifier->push_back("ReLU2", torch::nn::ReLU(torch::nn::ReLUOptions(true)));
+    this->classifier->push_back("Dropout3", torch::nn::Dropout());
+    this->classifier->push_back("Linear4", torch::nn::Linear(torch::nn::LinearOptions(4096, 4096)));
+    this->classifier->push_back("ReLU5", torch::nn::ReLU(torch::nn::ReLUOptions(true)));
+    this->classifier->push_back("Dropout6", torch::nn::Dropout());
+    this->classifier->push_back("Linear7", torch::nn::Linear(torch::nn::LinearOptions(4096, num_classes)));
 
     features = register_module("features", features);
+    avgpool = register_module("avgpool", avgpool);
     classifier = register_module("classifier", classifier);
 
     if (init_weights)
@@ -35,38 +58,9 @@ void VGGImpl::_initialize_weights()
 torch::Tensor VGGImpl::forward(torch::Tensor x)
 {
     x = features->forward(x);
-    x = avgpool(x);
+    x = avgpool->forward(x);
     x = x.flatten(1);
     x = classifier->forward(x);
     return x;
-}
-
-torch::nn::Sequential make_layers(const std::vector<int> &cfg, bool batch_norm)
-{
-    torch::nn::Sequential layers;
-    int in_channels = 3;
-    for (auto v : cfg)
-    {
-        if (v == -1) // stands for M
-        {
-            layers->push_back(torch::nn::MaxPool2d(torch::nn::MaxPool2dOptions(2)));
-        }
-        else
-        {
-            layers->push_back(torch::nn::Conv2d(torch::nn::Conv2dOptions(in_channels, v, 3).padding(1)));
-            if (batch_norm)
-            {
-                layers->push_back(torch::nn::BatchNorm2d(torch::nn::BatchNorm2dOptions(v)));
-            }
-            layers->push_back(torch::nn::ReLU(torch::nn::ReLUOptions(true)));
-            in_channels = v;
-        }
-    }
-    return layers;
-}
-
-VGG make_vgg16(int num_classes, bool pretrained)
-{
-    return VGG(make_layers(cfg_D, false), num_classes, !pretrained);
 }
 } // namespace backbone
--- backbone/vgg.h
+++ backbone/vgg.h
@@ -4,25 +4,19 @@
 
 namespace backbone
 {
-
+/// this is a special case vgg16D model, for reusing in faster rcnn model
 struct VGGImpl : public torch::nn::Module
 {
-    VGGImpl(torch::nn::Sequential features, int num_classes = 1000, bool init_weights = true);
+    VGGImpl(int num_classes = 1000, bool init_weights = true);
     torch::Tensor forward(torch::Tensor x);
 
     void _initialize_weights();
+
     torch::nn::Sequential features;
     torch::nn::AdaptiveAvgPool2d avgpool{nullptr};
     torch::nn::Sequential classifier;
 };
 
 TORCH_MODULE(VGG);
-
-torch::nn::Sequential make_layers(const std::vector<int> &cfg, bool batch_norm = false);
-
-const std::vector<int> cfg_D = {
-    64, 64, -1 /* stands for M */, 128, 128, -1, 256, 256, 256, -1, 512, 512, 512, -1, 512, 512, 512, -1};
-
-VGG make_vgg16(int num_classes, bool pretrained = true);
 
 } // namespace backbone
