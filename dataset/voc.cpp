200e3bbca6ea4c67c5adaa7daf86e67d8c3b630e
wr <wr666@mail.ustc.edu.cn>
2022-11-30 06:14:50 +0000

    use static random var

--- dataset/voc.cpp
+++ dataset/voc.cpp
@@ -142,9 +142,9 @@ void VOCDataset::transform(ExampleType &example, cv::Mat &image_data)
     if (mode == Mode::train)
     {
         float flip_ratio = 0.5f;
-        std::random_device rd;
-        std::default_random_engine eng(rd());
-        std::uniform_real_distribution<float> distr(0.0f, 1.0f);
+        static std::random_device rd;
+        static std::default_random_engine eng(rd());
+        static std::uniform_real_distribution<float> distr(0.0f, 1.0f);
         if (distr(eng) < flip_ratio)
         {
             std::vector<int64_t> img_shape({img.rows, img.cols});
